b35f946f6361cb76f0f7dfef4c0eaf846ace1073
Muhammad Mubasahar <muhammadmubasharoffical@gmail.com>
2025-05-19 11:59:51 +0500

    Update page.tsx

--- app/home4/page.tsx
+++ app/home4/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>"Lorm" refers to a communication system for deafblind people, a tactile alphabet developed in the late 19th century. It involves using taps, strokes, and other movements on the palm of a hand to represent letters, according to Wikipedia and other symbols. The Lorm alphabet is named after its inventor, Hieronymus Lorm, according to Wikipedia and is primarily used in German-speaking countries and other parts of Europe. 
+</div>
   )
 }
